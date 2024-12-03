84167d56bb68e2cd0e86c15d69a5486febd8dc97
Priyanshu Raj <pkrr6055224@gmail.com>
2024-12-03 22:40:59 +0530

    codecolabproject

--- server.js
+++ server.js
@@ -1,33 +1,35 @@
 const express = require('express');
-const app = express();
-const http = require('http');
 const path = require('path');
+const http = require('http');
 const { Server } = require('socket.io');
 const ACTIONS = require('./src/Actions');
 
+const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
 
-app.use(express.static('build'));
-app.use((req, res, next) => {
+const io = new Server(server, {
+    cors: {
+        origin: '*',
+        methods: ['GET', 'POST'],
+    },
+});
+
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 const userSocketMap = {};
+
 function getAllConnectedClients(roomId) {
-    // Map
-    return Array.from(io.sockets.adapter.rooms.get(roomId) || []).map(
-        (socketId) => {
-            return {
-                socketId,
-                username: userSocketMap[socketId],
-            };
-        }
-    );
+    return Array.from(io.sockets.adapter.rooms.get(roomId) || []).map((socketId) => ({
+        socketId,
+        username: userSocketMap[socketId],
+    }));
 }
 
 io.on('connection', (socket) => {
-    console.log('socket connected', socket.id);
+    console.log('Socket connected:', socket.id);
 
     socket.on(ACTIONS.JOIN, ({ roomId, username }) => {
         userSocketMap[socket.id] = username;
@@ -55,13 +57,12 @@ io.on('connection', (socket) => {
         rooms.forEach((roomId) => {
             socket.in(roomId).emit(ACTIONS.DISCONNECTED, {
                 socketId: socket.id,
-                username: userSocketMap[socket.id],
+                username: userSocketMap[socketId],
             });
         });
         delete userSocketMap[socket.id];
-        socket.leave();
     });
 });
 
 const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
